990d3e6ee23ec7c05c2ac6035f2193d9c72a127f
Daniel Lopes <dalopes@id-logistics.com>
2024-03-03 22:17:03 -0300

    variables and mutability chapter

---
+++ target/debug/deps/variables-2d9043d103c4166b.d
@@ -0,0 +1,5 @@
+C:\Users\danie\OneDrive\Área de Trabalho\RUST PROJECTS\rustbook_followup\variables\target\debug\deps\libvariables-2d9043d103c4166b.rmeta: src\main.rs
+
+C:\Users\danie\OneDrive\Área de Trabalho\RUST PROJECTS\rustbook_followup\variables\target\debug\deps\variables-2d9043d103c4166b.d: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/variables-e5116f01dad80151.d
@@ -0,0 +1,5 @@
+C:\Users\danie\OneDrive\Área de Trabalho\RUST PROJECTS\rustbook_followup\variables\target\debug\deps\libvariables-e5116f01dad80151.rmeta: src\main.rs
+
+C:\Users\danie\OneDrive\Área de Trabalho\RUST PROJECTS\rustbook_followup\variables\target\debug\deps\variables-e5116f01dad80151.d: src\main.rs
+
+src\main.rs:
